bc22b9c30d99455af2dd06a8fad3466efc46e795
Abdullah Al Abid <105044291+abid0-07@users.noreply.github.com>
2025-11-15 18:43:53 +0600

    Comment out DownLoadResumeBtn in page.tsx
    Comment out the DownLoadResumeBtn component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
           <div className="h-fit w-full p-4 flex gap-4 max-lg:justify-center max-sm:p-2 max-sm:justify-center">
             <SocialLinks />
           </div>
-          <DownLoadResumeBtn />
+          {/* <DownLoadResumeBtn /> */}
         </FramerWrapper>
         {/* RIGHT SIDE image  */}
         <FramerWrapper
